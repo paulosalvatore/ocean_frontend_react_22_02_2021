c9a186bbee81f64603ef8ceb18f388ee46f1d8a2
Paulo Salvatore <salvatoregames@gmail.com>
2021-02-22 21:34:56 -0300

    Condição de vitória

--- src/index.js
+++ src/index.js
@@ -91,5 +91,28 @@ ReactDOM.render(
 );
 
 function calculateWinner(squares) {
-    return true;
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+
+        const a = line[0];
+        const b = line[1];
+        const c = line[2];
+
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return true;
+        }
+    }
+
+    return false;
 }
